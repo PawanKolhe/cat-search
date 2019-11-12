31cf5a54198c2b1f60c00527f9b5afd8aa0f4263
Pawan Kolhe <contact@pawankolhe.com>
2019-11-12 18:58:12 +0530

    Updates

---
+++ precache-manifest.f511b8d112534417f27e365df6c35e37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a390645af5d46569dbba01f4dbe3370e",
+    "url": "/cat-search/index.html"
+  },
+  {
+    "revision": "3cdf819ec186c66b13fa",
+    "url": "/cat-search/static/css/main.1cf091cd.chunk.css"
+  },
+  {
+    "revision": "2542a2456ebbb1b1e5c7",
+    "url": "/cat-search/static/js/2.0ae589f9.chunk.js"
+  },
+  {
+    "revision": "3cdf819ec186c66b13fa",
+    "url": "/cat-search/static/js/main.25436117.chunk.js"
+  },
+  {
+    "revision": "f237080488c9c1c4bdd4",
+    "url": "/cat-search/static/js/runtime-main.5e9191df.js"
+  }
+]);
